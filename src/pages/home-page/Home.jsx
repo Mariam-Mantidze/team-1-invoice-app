c707625cc22e1f934ff7fac1ee248c0ba7fbf8ef
Mariam <mariam.mantidze1@gmail.com>
2024-05-14 16:09:50 +0400

    feat: find single invoice with corresponding id

---
+++ src/pages/home-page/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { invoiceContext } from "../../App";
+
+export default function Home() {
+  const { invoiceData, setInvoiceData } = useContext(invoiceContext);
+
+  return (
+    <>
+      {invoiceData.map((invoice) => {
+        return (
+          <ul style={{ border: "1px solid black" }} key={invoice.id}>
+            <li>
+              <Link to={`/${invoice.id}`}>{invoice.id}</Link>
+              <p>{invoice.status}</p>
+              <p>{invoice.clientName}</p>
+            </li>
+          </ul>
+        );
+      })}
+      <Link to={"/"}>New</Link>
+    </>
+  );
+}
--- src/single-invoice/SingleInvoice.jsx
+++
@@ -1,3 +0,0 @@
-export default function SingleInvoice() {
-  return <>Single Invoice</>;
-}
