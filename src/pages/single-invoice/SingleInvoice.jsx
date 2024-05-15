b335c60a265860bc1c121e35ec8b02a8fffa1519
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-15 13:13:50 +0400

    feat: add status div to single invoice

--- src/pages/single-invoice/SingleInvoice.jsx
+++ src/pages/single-invoice/SingleInvoice.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { invoiceContext } from "../../App";
 import { useParams } from "react-router-dom";
 import GoBack from "../../GoBack";
@@ -12,10 +12,47 @@ export default function SingleInvoice() {
   // find single invoice with corresponding id
   const invoice = invoiceData.find((invoice) => invoice.id === id);
 
+  const [statusColor, setStatusColor] = useState("#373B53");
+
+  const handleStatusColor = () => {
+    if (invoice.status === "pending") {
+      setStatusColor("#FF8F00");
+    } else if (invoice.status === "paid") {
+      setStatusColor("#33D69F");
+    } else {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    handleStatusColor();
+  }, [invoice.status]);
+
   return (
-    <>
-      <p>{invoice.id}</p>
+    <div className="flex flex-col p-6 bg-gray-50 min-h-screen">
       <GoBack />
-    </>
+      <div>
+        <div className="flex p-6 justify-between items-center rounded-md shadow-sm bg-white">
+          <p className="text-gray-400 text-sm font-medium leading-5 tracking-tight">
+            Status
+          </p>
+          <div
+            className="flex items-center gap-2 py-[12px] px-[18px] rounded-lg "
+            style={{ backgroundColor: `${statusColor}57` }}
+          >
+            <div
+              className="w-2 h-2 rounded-full"
+              style={{ backgroundColor: statusColor }}
+            ></div>
+            <p
+              className="text-base font-semibold leading-4 tracking-tight"
+              style={{ color: statusColor }}
+            >
+              {invoice.status}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
