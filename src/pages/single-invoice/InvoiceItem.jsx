e1518a585a0de8cc26df4e161613f5990a9ace2b
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-15 23:50:39 +0400

    style: add responsive for tablet

--- src/pages/single-invoice/InvoiceItem.jsx
+++ src/pages/single-invoice/InvoiceItem.jsx
@@ -1,13 +1,15 @@
 export default function InvoteItem({ name, quantity, price, total }) {
   return (
     <div className="flex justify-between">
-      <div className="flex flex-col">
+      <div className="flex flex-col md:flex-row md:justify-between md:w-[70%]">
         <p className="text-gray-900 text-lg font-bold leading-5 tracking-tight">
           {name}
         </p>
-        <p className="text-gray-400 text-sm font-medium leading-5 tracking-tight">
-          {quantity} <span>x</span> £ {price}
-        </p>
+
+        <div className="flex text-gray-400 text-sm font-medium leading-5 tracking-tight gap-1 md:justify-between md:w-[48%]">
+          <p>{quantity}</p> <span className="md:hidden">x</span>{" "}
+          <p>£ {price}</p>
+        </div>
       </div>
       <p className="text-gray-900 text-lg font-bold leading-5 tracking-tight">
         £ {total}
