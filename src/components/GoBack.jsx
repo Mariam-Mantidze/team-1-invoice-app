1035307b3284963c3a09722fa5e7718750a23c2b
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-15 15:36:50 +0400

    fix: add routing to go back component

--- src/components/GoBack.jsx
+++ src/components/GoBack.jsx
@@ -1,12 +1,13 @@
 import Arrowleft from "/assets/icon-arrow-left.svg";
+import { Link } from "react-router-dom";
 
 export default function GoBack() {
   return (
-    <div className="flex items-center gap-4">
+    <Link to="/" className="flex items-center gap-4 cursor-pointer">
       <img src={Arrowleft} alt="left arrow icon" />
       <p className="text-gray-900 text-lg font-bold leading-tight tracking-tight">
         Go back
       </p>
-    </div>
+    </Link>
   );
 }
