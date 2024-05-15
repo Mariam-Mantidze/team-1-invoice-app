c303d4ccf86afa3d258606fe938dba289fd4174b
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-15 14:02:56 +0400

    style: style invoice dates component

--- src/components/SingleInvoice/InvoiceDates.jsx
+++ src/components/SingleInvoice/InvoiceDates.jsx
@@ -1,8 +1,12 @@
 export default function InvoiceDates({ name, date }) {
   return (
-    <div>
-      <p>{name}</p>
-      <p>{date}</p>
+    <div className="flex flex-col gap-[13px]">
+      <p className="text-gray-400 text-sm font-medium leading-5 tracking-tight">
+        {name}
+      </p>
+      <p className="text-gray-900 text-lg font-bold leading-5 tracking-tight">
+        {date}
+      </p>
     </div>
   );
 }
