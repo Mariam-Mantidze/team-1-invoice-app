bdf089756ffe221a50873ac5d67b8115b221e058
lashasaata <lashasaata@gmail.com>
2024-05-16 05:46:25 -0700

    refactor: add function that implement dato into correct way and fixes total price to display

--- src/pages/home-page/components/Main.jsx
+++ src/pages/home-page/components/Main.jsx
@@ -1,12 +1,29 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { invoiceContext } from "../../../App";
 
 function Main() {
   const content = useContext(invoiceContext);
-  console.log(content.invoiceData);
+
+  // using useref and useEffect to implement correct date
+  const dateRef = useRef([]);
+  useEffect(() => {
+    const dates = content.invoiceData.map((e) => {
+      const datestr = e.paymentDue;
+      const parts = datestr.split("-");
+      const date = new Date(parts[0], parts[1] - 1, parts[2]);
+      const month = date.toLocaleString("en-us", { month: "long" });
+      const day = date.getDate();
+      const year = date.getFullYear();
+      const fulldate = `${day} ${month} ${year}`;
+      return { payDue: fulldate };
+    });
+    dateRef.current = dates;
+  }, [content.invoiceData]);
+
+  //   const fixedAmount = content.invoiceData.
   return (
     <main>
-      {content.invoiceData.map((e) => {
+      {content.invoiceData.map((e, index) => {
         return (
           <section key={e.id}>
             <div>
@@ -15,8 +32,11 @@ function Main() {
             </div>
             <div>
               <div>
-                <p>{e.paymentDue}</p>
-                <span>£ {e.total}</span>
+                <p>
+                  due{" "}
+                  {dateRef.current[index] ? dateRef.current[index].payDue : ""}
+                </p>
+                <span>£ {Number(e.total).toFixed(2)}</span>
               </div>
               <div>
                 <div></div>
