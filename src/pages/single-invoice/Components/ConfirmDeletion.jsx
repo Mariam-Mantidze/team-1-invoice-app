b0cfd90082935c1383515e69629231a3a8e956cd
mariambuzaladze <mariambuzaladze10@example.come>
2024-05-17 22:45:31 +0400

    wip: cancel button

---
+++ src/pages/single-invoice/Components/ConfirmDeletion.jsx
@@ -0,0 +1,32 @@
+import DeleteButton from "./DeleteButton";
+
+export default function ConfirmDeletion({ id, showDelete, setShowDelete }) {
+  return (
+    <div
+      className={`${
+        showDelete ? "flex" : "hidden"
+      } absolute top-[30%] left-[30%] flex-col p-12 bg-[#FFF] shadow-lg rounded-md gap-4`}
+    >
+      <p className="text-[#0C0E16] text-lg font-bold leading-5 tracking-tight">
+        Confirm Deletation
+      </p>
+      <p className="text-[#858BB2] text-sm font-medium leading-5 tracking-tight">
+        Are you sure you want to delete invoice #{id}? This action cannot be
+        undone.
+      </p>
+
+      <div className="flex justify-end gap-2">
+        <button
+          onClick={() => {
+            setShowDelete(false);
+            console.log("hgj");
+          }}
+          className="rounded-full bg-[#F9FAFE] py-4 px-6 text-[#7E88C3] text-sm font-bold leading-tight tracking-tight hover:bg-[#DFE3FA]"
+        >
+          Cancel
+        </button>
+        <DeleteButton />
+      </div>
+    </div>
+  );
+}
