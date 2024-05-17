16c6a8e97ad938cfda44ea9c53af6dbd265984e1
lashasaata <lashasaata@gmail.com>
2024-05-17 06:36:40 -0700

    feat: add filter section and basic structure of it

--- src/pages/home-page/Home.jsx
+++ src/pages/home-page/Home.jsx
@@ -5,9 +5,15 @@ import Main from "./components/Main";
 
 export default function Home() {
   const [filteredData, setFilteredData] = useState({});
+  const [activeFilter, setActiveFilter] = useState(false);
+
   return (
     <div className="flex flex-col items-center gap-8">
-      <Heading />
+      <Heading
+        setFilteredData={setFilteredData}
+        activeFilter={activeFilter}
+        setActiveFilter={setActiveFilter}
+      />
       <Main />
     </div>
   );
--- src/pages/home-page/components/Heading.jsx
+++ src/pages/home-page/components/Heading.jsx
@@ -1,8 +1,12 @@
 import { useContext } from "react";
 import { invoiceContext } from "../../../App";
 
-function Heading() {
+function Heading(props) {
   const context = useContext(invoiceContext);
+  const activateFilter = () => {
+    props.setActiveFilter(!props.activeFilter);
+  };
+
   return (
     <header className=" w-[327px] flex justify-between items-center mt-8">
       <div>
@@ -15,11 +19,36 @@ function Heading() {
         </p>
       </div>
       <div className="flex items-center gap-[18.5px]">
-        <div className="flex items-center gap-3">
-          <span className="text-[15px] text-[#0c0e16] font-[700] tracking-[-0.25px] leading-[1]">
+        <div className="flex items-center gap-3 relative">
+          <span
+            onClick={activateFilter}
+            className="text-[15px] text-[#0c0e16] font-[700] tracking-[-0.25px] leading-[1]"
+          >
             Filter
           </span>
-          <img src="/public/assets/icon-arrow-down.svg" alt="arrow_down" />
+          <img
+            className={props.activeFilter ? "rotate-180" : ""}
+            src="/public/assets/icon-arrow-down.svg"
+            alt="arrow_down"
+          />
+          {props.activeFilter ? (
+            <div className="w-[90px] h-20 rounded-[8px] bg-[#fff] shadow-filter absolute bottom-[-98px] left-[-16px]">
+              <div>
+                <input type="checkbox" />
+                <label htmlFor="">Draft</label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label htmlFor="">Pending</label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label htmlFor="">Paid</label>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         <button
           onClick={() => context.navigate(`/new-invoice`)}
