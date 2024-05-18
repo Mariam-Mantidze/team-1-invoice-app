5bf0ed84780fe60d851d63f69c0fca43840c0f34
lashasaata <lashasaata@gmail.com>
2024-05-18 09:25:58 -0700

    style: dark mode style for heading and main components

--- src/pages/home-page/components/Main.jsx
+++ src/pages/home-page/components/Main.jsx
@@ -34,22 +34,22 @@ function Main(props) {
             <section
               key={e.id}
               onClick={() => content.navigate(`/${e.id}`)}
-              className="w-[327px] md:w-[672px] lg:w-[730px] flex flex-col md:flex-row md:justify-between md:items-center gap-6 md:gap-0 bg-white rounded-[8px] shadow-invoice pt-[25px] px-6 pb-[22px] md:py-4 lg:pl-8 hover:border hover:border-solid hover:border-[#7c5dfa] hover:cursor-pointer"
+              className="w-[327px] md:w-[672px] lg:w-[730px] flex flex-col md:flex-row md:justify-between md:items-center gap-6 md:gap-0 bg-white dark:bg-[#1e2139] rounded-[8px] shadow-invoice dark:shadow-darkInvoice pt-[25px] px-6 pb-[22px] md:py-4 lg:pl-8 hover:border hover:border-solid hover:border-[#7c5dfa] hover:cursor-pointer"
             >
               <div className="flex justify-between items-center md:gap-[40px] lg:gap-[48px]">
                 <div className="md:w-[180px] lg:w-[195px] flex justify-between">
-                  <h3 className="text-[15px] text-[#0c0e16] font-[700] tracking-[-0.25px] leading-[1]">
+                  <h3 className="text-[15px] text-[#0c0e16] dark:text-[#fff] font-[700] tracking-[-0.25px] leading-[1]">
                     <span className="text-[#7e88c3]">#</span>
                     {e.id}
                   </h3>
-                  <p className="text-[13px] text-[#7e88c3] font-[500] tracking-[-0.1px] leading-[1.15] hidden md:flex">
+                  <p className="text-[13px] text-[#7e88c3] dark:text-[#dfe3fa] font-[500] tracking-[-0.1px] leading-[1.15] hidden md:flex">
                     due{" "}
                     {dateRef.current[index]
                       ? dateRef.current[index].payDue
                       : ""}
                   </p>
                 </div>
-                <span className="text-[13px] text-[#858bb2] font-[500] tracking-[-0.1px] leading-[1.15] md:ml-[23px]">
+                <span className="text-[13px] text-[#858bb2] dark:text-[#fff] font-[500] tracking-[-0.1px] leading-[1.15] md:ml-[23px]">
                   {e.clientName}
                 </span>
               </div>
@@ -61,7 +61,7 @@ function Main(props) {
                       ? dateRef.current[index].payDue
                       : ""}
                   </p>
-                  <span className="text-[15px] text-[#0c0e16] font-[700] tracking-[-0.25px] leading-[1.6] md:mr-5">
+                  <span className="text-[15px] text-[#0c0e16] dark:text-[#fff] font-[700] tracking-[-0.25px] leading-[1.6] md:mr-5">
                     £ {Number(e.total).toFixed(2)}
                   </span>
                 </div>
@@ -71,7 +71,7 @@ function Main(props) {
                       ? "bg-green"
                       : e.status == "pending"
                       ? "bg-orange"
-                      : "bg-black"
+                      : "bg-black dark:bg-grey"
                   } w-[104px] h-10 flex justify-center items-center gap-2 rounded-[8px] opacity-transparent pt-[14px] pb-[11px]`}
                 >
                   <div
@@ -80,7 +80,7 @@ function Main(props) {
                         ? "bg-[#33d69f]"
                         : e.status == "pending"
                         ? "bg-[#ff8f00]"
-                        : "bg-[#373b53]"
+                        : "bg-[#373b53] dark:bg-[#dfe3fa]"
                     } w-2 h-2 rounded-full`}
                   ></div>
                   <span
@@ -89,7 +89,7 @@ function Main(props) {
                         ? "text-[#33d69f]"
                         : e.status == "pending"
                         ? "text-[#ff8f00]"
-                        : "text-[#373b53]"
+                        : "text-[#373b53] dark:text-[#dfe3fa]"
                     } text-[13px] font-[700] tracking-[-0.1px] leading-[1.15]`}
                   >
                     {e.status}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ export default {
     boxShadow: {
       invoice: "0 10px 10px -10px rgba(72, 84, 159, 0.1)",
       filter: "0 10px 20px 0 rgba(72, 84, 159, 0.25)",
+      darkInvoice: "0 10px 10px -10px rgba(72, 84, 159, 0.1)",
+      darkFilter: " 0 10px 20px 0 rgba(0, 0, 0, 0.25)",
     },
     colors: {
       green: "rgba(51, 214, 159, 0.06)",
       orange: "rgba(255, 143, 0, 0.06)",
       black: "rgba(55, 59, 83, 0.06)",
+      grey: "rgba(223, 227, 250, 0.06)",
     },
   },
   darkMode: "class",
